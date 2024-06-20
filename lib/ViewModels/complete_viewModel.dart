66c9bfb7119f9c86f2dfe91f9aeb8945dca234a8
Vincent_Ivan <vpalomata25@gmail.com>
2024-06-20 18:58:45 +0800

    change codebase structure

---
+++ lib/ViewModels/complete_viewModel.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:bucketcheck/Models/check_list_model.dart';
+
+class CompletedViewModel extends ChangeNotifier{
+
+  List<CheckList> completedLists = [];
+
+
+  Widget displayCompleted(i){
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 15, 8, 8),
+      child: Row(
+        children: [
+          
+          Expanded(
+            flex: 0,
+            child: Checkbox(
+              activeColor: const Color.fromRGBO(139, 203, 176,1),
+              value: completedLists[i].isChecked,
+              onChanged: (value) => (),
+              ),
+            ),
+
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("${completedLists[i].checkListName}", style: const TextStyle(fontWeight: FontWeight.bold, decoration: TextDecoration.lineThrough, fontSize: 18),),
+                Text("${completedLists[i].place}", style: const TextStyle(fontSize: 12),),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text("${completedLists[i].checkListDate}", style: const TextStyle(fontSize: 12),),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void deleteCheck(i){
+    completedLists.removeAt(i);
+    notifyListeners();
+    
+  }
+
+  void clearAll(){
+    completedLists.clear();
+    notifyListeners();
+  }
+} 
