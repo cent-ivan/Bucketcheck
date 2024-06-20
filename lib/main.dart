4c41f4616eeb8c49eed8a1c6f9dc3e8422dc9a18
Vincent_Ivan <vpalomata25@gmail.com>
2024-06-20 18:59:02 +0800

    space changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:bucketcheck/ViewModels/active_viewModel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() { 
   runApp(
     MultiProvider(
       providers: [
